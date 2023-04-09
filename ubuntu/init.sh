267ec015671abddf97f4c66e126bd63f9b11eb83
wacdev <i@wac.tax>
2023-04-09 08:25:03 +0800

    ♨

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -30,6 +30,10 @@ if [ -n "$GFW" ]; then
   sed -i "s/archive.ubuntu.com/mirrors.163.com/g" /etc/apt/sources.list
   rsync -avI $ZHOS/ /
   source $ZHOS/root/.export
+  apt-get install -y python
+  pip3 install apt-select
+  apt-select --country CN
+  mv /root/sources.list /etc/apt/
 fi
 
 systemctl stop snapd || true
