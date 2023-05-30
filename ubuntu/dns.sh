217e20e8045d0b2f01044c1c3e654b8d3f04f34d
wacdev <i@wac.tax>
2023-05-30 13:09:10 +0800

    ♨

--- ubuntu/dns.sh
+++ ubuntu/dns.sh
@@ -4,7 +4,8 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+cp $DIR/dns.conf /etc
+cd /tmp
 git clone --depth=1 https://github.com/mokeyish/smartdns-rs.git
 cd smartdns-rs
 cargo build --release
-cp $DIR/dns.conf /etc
