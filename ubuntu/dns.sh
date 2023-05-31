33e99e8fc1810a6ff11f8e49ae0a089107de02c6
wacdev <i@wac.tax>
2023-05-31 15:44:45 +0800

    ♨

--- ubuntu/dns.sh
+++ ubuntu/dns.sh
@@ -4,8 +4,25 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-cp $DIR/dns.conf /etc
 cd /tmp
-git clone --depth=1 https://github.com/mokeyish/smartdns-rs.git
-cd smartdns-rs
+
+if [ -d "smartdns-rs" ]; then
+  cd smartdns-rs
+  git pull
+else
+  git clone --depth=1 https://github.com/mokeyish/smartdns-rs.git
+  cd smartdns-rs
+fi
+
 cargo build --release
+
+CONF=/etc/smartdns/smartdns.conf
+if [ ! -f "$CONF" ]; then
+  mkdir -p /etc/smartdns
+  cp .$CONF $CONF
+  sed -i -e "s/^# server https.*/server 1.0.0.1\nserver 8.8.4.4\nserver 1.1.1.1\nserver 8.8.8.8/" $CONF
+fi
+
+cp target/release/smartdns /usr/local/bin/smartdns
+smartdns service install
+systemctl enable smartdns-rs --now
