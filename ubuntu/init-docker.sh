d5fd22cacc66b9e2c1f3bcaa815ce600d3e51fcb
wacdev <i@wac.tax>
2023-08-14 16:53:04 +0800

    ♨

--- ubuntu/init-docker.sh
+++ ubuntu/init-docker.sh
@@ -6,7 +6,7 @@ apt-get update
 #fi
 #curl -fsSL https://get.docker.com | sudo bash -s docker $mirror
 pip3 install --force-reinstall PyYAML==5.3.1
-curl -fsSL https://get.docker.com | sudo bash -s docker
+curl -fsSL https://get.docker.com | bash -s docker
 systemctl start docker
 systemctl enable docker
 pip3 install docker-compose
